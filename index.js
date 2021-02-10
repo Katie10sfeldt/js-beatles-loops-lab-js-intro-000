7b2b23378b857c15e67c0b59f6615bca7f0b1776
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 17:09:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 function theBeatlesPlay(arrMus, arrInst) {
   var array = [];
   for(let i = 0; i > arrMus.length; i++) {
-    array.push(`${arrMus[i]} plays ${arrInst[i]}`);
+    array.push(arrMus[i] + ' plays ' + arrInst[i]);
   }
   return array;
 }
