aacbeb89a8863c9e009494dbcd8c0c0d5361c61d
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 17:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+// Lab Problem 1: theBeatlesPlay
+
+function theBeatlesPlay(arrMus, arrInst) {
+  var array = [];
+  for(let i = 0; i > arrMus.length; i++) {
+    array.push(`${arrMus[i]} plays ${arrInst[i]}`);
+  }
+  return array;
+}
