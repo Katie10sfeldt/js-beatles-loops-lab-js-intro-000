101c57130272cdee4b3b1f14d4ba8fdec505963a
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 17:22:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ function johnLennonFacts(array) {
 
 function iLoveTheBeatles(num) {
   var array = [];
-  let i = 1;
+
   function incrementVariable() {
+    let i = 1;
     i = i + 1;
     return i;
   }
